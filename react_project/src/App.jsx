855fc7c2574ac315bbb3360c03880efe6599af30
MCHAITANYANAIDU <mchaitanyaprasad65@gmail.com>
2024-10-26 13:11:44 +0530

    new

---
+++ react_project/src/App.jsx
@@ -0,0 +1,11 @@
+import Hell from "./components/new";
+
+function App(){
+  return (
+    <>
+   <Hell/>
+    </>
+  );    
+}
+
+export default App;
---
+++ react_project/src/components/new.jsx
@@ -0,0 +1,13 @@
+
+import "./new.css"
+
+function Hell(){
+    return (
+        <>
+        <h1>HI THIS IS CHAITANYA</h1>
+        </>
+    )
+
+}
+
+export default Hell;
